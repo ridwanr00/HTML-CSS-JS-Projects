0193d3b4b02dad684f373f18b510b54d5b90b07c
ridwanr00 <rasyid.r00@gmail.com>
2025-01-30 19:23:43 +0700

    character counter

--- 03 characterCounter/script.js
+++ 03 characterCounter/script.js
@@ -1,10 +1,14 @@
 let totalCharacter = 0;
 
 const counter = () => {
-    const words = document.getElementById('paragraph').value
-    const total = words.split('')
-    console.log(total.length)
-    totalCharacter = total.length
-    document.getElementById('count').innerHTML = `character count: ${totalCharacter}`
-    document.getElementById('remaining').innerHTML = `character remaining: ${1000 - totalCharacter}`
-}
+  const words = document.getElementById("paragraph").value;
+  const total = words.split("");
+  console.log(total.length);
+  totalCharacter = total.length;
+  document.getElementById(
+    "count"
+  ).innerHTML = `character count: ${totalCharacter}`;
+  document.getElementById("remaining").innerHTML = `character remaining: ${
+    1000 - totalCharacter
+  }`;
+};
